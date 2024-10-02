f627e9ebb48e1c9130b6f1e2acdb1f7365b049c6
PBW <bottlerstudy3123@gmail.com>
2024-10-02 17:40:55 +0900

    first commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+    constructor() {
+        this.client_id = 'e932ba1d163e2d200af4';
+        this.client_secret = '0e1ae9f3a7ec4b8dbdf6ab4108dd7af75030dfbb';
+        this.repos_count = 5;
+        this.repos_sort = 'created : asc';
+    }
+    
+    async getUser(text) {
+        const profileResponse = await fetch(
+            `https://api.github.com/users/${text}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+    
+        const repoResponse = await fetch(
+            `https://api.github.com/users/${text}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+    
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+    
+        return {
+            profile,
+            repos
+        }
+    }
+}
